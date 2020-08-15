2e70fab366c20c665137c2c4853d998e539c6a38
mister-blanket <michael@mrplunkett.com>
2020-08-15 00:09:23 -0500

    Raised text

--- js/movingShadow.js
+++ js/movingShadow.js
@@ -54,25 +54,55 @@
   };
 
   const makeShadow = (element, xDiff, yDiff, farthestPoint, settings) => {
-    const { angle, diffusion, color } = settings;
+    const { angle, diffusion, color, fixedShadow } = settings;
 
     let shadowArr = [];
 
+    // If fixed shadow, add
+    fixedShadow && shadowArr.push(fixedShadow);
+
+    // Set offset values
+    let xOffset = settings.xOffset ? settings.xOffset : 0;
+    let yOffset = settings.yOffset ? settings.yOffset : 0;
+
     for (let i = angle; i < (farthestPoint + angle); i++) {
-      shadowArr.push(`${-xDiff/i}px ${-yDiff/i}px ${diffusion}px ${color}`);
+      shadowArr.push(`${(-xDiff/i)+xOffset}px ${(-yDiff/i)+yOffset}px ${diffusion}px ${color}`);
     }
 
     element.style.textShadow = shadowArr.join();
   };
 
+  const makeDropShadow = (element, xDiff, yDiff, farthestPoint, settings) => {
+    const { angle, diffusion, color, fixedShadow } = settings;
+
+    let shadowArr = [];
+
+    // If fixed shadow, add
+    fixedShadow && shadowArr.push(fixedShadow);
+
+    // Set offset values
+    let xOffset = settings.xOffset ? settings.xOffset : 0;
+    let yOffset = settings.yOffset ? settings.yOffset : 0;
+
+    shadowArr.push(`${(-xDiff/angle)+xOffset}px ${(-yDiff/angle)+yOffset}px ${diffusion}px ${color}`);
+
+    element.style.textShadow = shadowArr.join();
+  };
+
   const movingShadow = settings => {
+
+      // Select element
+      var elements = document.querySelectorAll(settings.selector);
+
+      // Set initial fixedShadow
+      settings.fixedShadow && elements.forEach(element => {
+        element.style.textShadow = settings.fixedShadow;
+      });
+
     document.onmousemove = e => handleMove(e, settings);
     document.ontouchmove = e => handleMove(e, settings);
 
     function handleMove(event, settings) {
-
-      // Select element
-      var elements = document.querySelectorAll(settings.selector);
 
       // Get mouse position
       const touchPos = getTouchPos(event);
@@ -89,11 +119,15 @@
           const yDiff = touchPos.y - elePos.centerY;
 
           // Determines furthes mouse point (x or y) from element
-          const farthestPoint = Math.round(Math.max(Math.abs(xDiff), Math.abs(yDiff))/4);
+          const farthestPointFactor = settings.type === "dropShadow" ? 40 : 4;
+          const farthestPoint = Math.round(Math.max(Math.abs(xDiff), Math.abs(yDiff))/farthestPointFactor);
 
           switch(settings.type) {
             case 'shadow':
               makeShadow(element, xDiff, yDiff, farthestPoint, settings);
+              break;
+            case 'dropShadow':
+              makeDropShadow(element, xDiff, yDiff, farthestPoint, settings);
               break;
             case 'perspective':
               console.log('perspective');
